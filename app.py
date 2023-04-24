b30009e0f60db6bc561af801ba7220e8a32a136e
siddharth1998 <siddharth1998@gmail.com>
2023-04-24 04:50:52 -0400

    added the post and get; delete is remaining

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask,request
+import threading
+import secrets
+
+from secrets import randbelow
+
+from datetime import timezone
+
+import datetime
+
+app = Flask(__name__)
+
+main_list=[]
+sem = threading.Semaphore()
+
+@app.get("/random/<int:sides>")
+def roll(sides):
+    if sides <= 0:
+        return { 'err': 'need a positive number of sides' }, 400
+    
+    return { 'num': randbelow(sides) + 1 }
+
+@app.post("/post")
+def post():
+    global main_list
+    if request.method=="POST":
+       
+       # can be converted into a function which will be used in other methods
+        if len((request.json.keys()))>1 or len((request.json.keys()))==0:
+            return {"err":"empty body sent"},400 
+        if "msg" in request.json:
+            pass
+        else:
+            return {"err":"msg not found"},400
+        if type(request.json["msg"])!=str:
+            return {"err":"msg value is not a str type"},400
+        sem.acquire()
+        flag=True
+        for i in main_list:
+            
+            if "id" in i:
+                temp_id=i["id"]
+                flag=False
+            else:
+                flag=True
+        
+        if flag==True:
+            temp_id=0
+            pass
+        else:
+            temp_id=temp_id+1
+            flag=False
+        
+        
+        temp_dict={"id":temp_id,"key":secrets.token_hex(32),"timestamp":datetime.datetime.now(timezone.utc).isoformat()}
+        main_list.append(temp_dict)
+        sem.release()
+        return temp_dict,200
+    else:
+        return {"err":"Parameters Not Inclucded or this resource not found"},404
+    
+def exsistence_checker(input_id):
+    global main_list
+    flag=False
+    for index,i in enumerate(main_list):
+        if input_id==i["id"]:
+            flag=True
+            temp_dict=i
+            break
+    if flag==False:
+        return False
+    else:
+        return temp_dict,index
+        # return {"err":"id not found"},404
+
+
+@app.get("/post/<int:input_id>")
+def get(input_id):
+    global main_list
+    
+    if request.method=="GET":
+        temp_dict=exsistence_checker(input_id)
+        if temp_dict==False:
+            return {"err":"id not found"},404
+        else:
+            temp_dict=temp_dict[0]
+            res={"id":input_id,"timestamp":temp_dict["timestamp"],"msg":temp_dict["key"]}
+            return res,200
+
+@app.delete("/post/<int:input_id>/delete/<string:input_key>")
+def delete(input_id,input_key):
+    if request.method=="DELETE":
+        temp_dict,index=exsistence_checker(input_id)
+        if input_id==False:
+            return {"err":"id not found"},404
+        else:
+            temp_dict=temp_dict[0]
+            sem.acquire()
+            
+            sem.release()
+        pass
