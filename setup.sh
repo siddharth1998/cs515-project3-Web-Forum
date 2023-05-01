86c739a2cdf7c4aabdcc42462e8c435a7cc2fd77
siddharth1998 <siddharth1998@gmail.com>
2023-05-01 17:45:17 -0400

    adding exec on line 25

--- setup.sh
+++ setup.sh
@@ -1,36 +1,38 @@
-#!/bin/sh
+    #!/bin/bash
 
-echo "Running the setup.sh"
+    echo "Running the setup.sh"
 
-service mongod status
+    service mongod status
 
-apt-get install gnupg -y
+    apt-get install gnupg -y
 
-apt install curl -y
+    apt install curl -y
 
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-   gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-   --dearmor
+    curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+    gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+    --dearmor
 
-touch /etc/apt/sources.list.d/mongodb-org-6.0.list
+    touch /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-apt-get update
+    apt-get update
 
-apt-get install -y mongodb-org
+    apt-get install -y mongodb-org
 
-mkdir -p /data/db
+    mkdir -p /data/db
 
-mongod &
+    exec mongod &    
 
-location_of_python=$(which python3)
+    # Trying with this 
 
-echo $location_of_python
+    location_of_python=$(which python3)
 
-# apt install python3-pip -y
-#above python pip can be removed if professor answers it 
+    echo $location_of_python
 
-pip3 install -r requirement.txt
+    # apt install python3-pip -y
+    #above python pip can be removed if professor answers it 
 
-# $location_of_python -m flask run 
+    pip3 install -r requirement.txt
+
+    # $location_of_python -m flask run 
