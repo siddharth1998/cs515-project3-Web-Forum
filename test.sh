7178af6d58bb5484f154c7638dbae04e3d358d6b
siddharth1998 <siddharth1998@gmail.com>
2023-05-01 16:34:27 -0400

    adding multiple interations of testcases

--- test.sh
+++ test.sh
@@ -10,6 +10,6 @@ exec flask run -p 5000 &
 
 echo "Running the test.sh"
 
-newman run users_collection.json -n 1
+newman run users_collection.json -n 5
 
-newman run cs515.postman_collection.json -n 1
+newman run cs515.postman_collection.json -n 5
